5cd634c1a6ccd2ca8b81027a4618078f702d78e0
Jatin Pandey <jatinpandey77@gmail.com>
2018-10-15 19:51:58 +0530

    Added article details page.

--- articles/urls.py
+++ articles/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from . import views
 
+app_name = 'articles'
+
 urlpatterns = [
-    path('', views.article_list),
-    path("<slug:slug>/", views.article_detail),
+    path('', views.article_list, name="list"),
+    path("<slug:slug>/", views.article_detail, name="detail"),
 ]
--- articles/views.py
+++ articles/views.py
@@ -5,5 +5,6 @@ from django.http import HttpResponse
 def article_list(request):
     articles = Article.objects.all().order_by('date');
     return render(request, 'articles/article_list.html', { 'articles': articles })
-def article_detail(request):
-    return HttpResponse('hello')
+def article_detail(request,slug):
+    article = Article.objects.get(slug=slug)
+    return render(request, 'articles/article_detail.html', { 'article': article })
