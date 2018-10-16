09dfc9c7f6a48e6965c5087f8ffb2ff4193c3789
Jatin Pandey <jatinpandey77@gmail.com>
2018-10-16 18:57:41 +0530

    Created Accounts App

--- djangonautic/urls.py
+++ djangonautic/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
-from . import views
 from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
     path('articles/', include('articles.urls')),
     path('about/', views.about),
     path('', views.home),
